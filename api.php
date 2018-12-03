9e664869e21aff28b7db3052047bcf7f064112c8
Tsogtbayar Davaadorj <acep.world@gmail.com>
2018-12-03 14:19:18 +0800

    initial commit

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $logs = file_get_contents("logs.json");
+    $json = json_decode($logs);
+    $newlog = file_get_contents('php://input');
+    array_push($json->logs, $newlog);
+    $logs = json_encode($json);
+    file_put_contents("logs.json", $logs);
+}
+
+$logs = file_get_contents("logs.json");
+
+echo $logs;
+
+?>
